4f20ab9dd1f2389997c8d507aa4ac2841d351c6f
Keinix <zackcosborn@gmail.com>
2018-10-30 18:58:00 +0900

    added firebase and MLkit to the project

---
+++ app/src/main/java/io/keinix/justthetext/main/domain/usecases/GetConvertedText.java
@@ -0,0 +1,14 @@
+package io.keinix.justthetext.main.domain.usecases;
+
+import android.graphics.Bitmap;
+
+import io.keinix.justthetext.data.ConvertedText;
+
+public class GetConvertedText {
+
+    public ConvertedText getConvertedText(Bitmap bitmap) {
+        return null;
+    }
+
+
+}
